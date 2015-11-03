70620d2f86cf65280c86ff7dfc19ec93cc5202bf
Dmitriy Raschepkin <dmitriy.raschepkin@gmail.com>
2015-11-03 11:03:27 +0200

    Cart Form - client fields added

--- lib/form/doctrine/ShopCartForm.class.php
+++ lib/form/doctrine/ShopCartForm.class.php
@@ -26,15 +26,40 @@ class ShopCartForm extends BaseShopCartForm
     unset($this['created_at'], $this['updated_at']);
     foreach($this->getObject()->getCartProducts() as $key => $cartProduct)
     {
-      $this->embedRelation('CartProducts', 'CartProductForm', [
-
+      $this->embedRelation(
+        'CartProducts',
+        'CartProductForm',
+        [
           [
             'request' => $this->options['request'],
             'response' => $this->options['response'],
             'user' => $this->options['user'],
             'action' => CartProductForm::ACTION_UPDATE,
           ]
-      ]);
+        ]
+      );
     }
+
+    $this->setWidget('firstname', new sfWidgetFormInputText(['label' => 'First name']));
+    $this->setWidget('lastname', new sfWidgetFormInputText(['label' => 'Last name']));
+    $this->setWidget('email', new sfWidgetFormInputText(['label' => 'Email']));
+    $this->setWidget('address1', new sfWidgetFormInputText(['label' => 'Address']));
+    $this->setWidget('city', new sfWidgetFormInputText());
+    $this->setWidget('state', new sfWidgetFormInputText());
+    $this->setWidget('postcode', new sfWidgetFormInputText(['label' => 'Post code']));
+    $this->setWidget('country', new sfWidgetFormI18nChoiceCountry(['label' => 'Country (2-letter code)']));
+    $this->setWidget('phonenumber', new sfWidgetFormInputText(['label' => 'Phone number']));
+    $this->setWidget('password2', new sfWidgetFormInputPassword(['label' => 'Password']));
+
+    $this->setValidator('firstname', new sfValidatorString());
+    $this->setValidator('lastname', new sfValidatorString());
+    $this->setValidator('email', new sfValidatorEmail());
+    $this->setValidator('address1', new sfValidatorString());
+    $this->setValidator('city', new sfValidatorString());
+    $this->setValidator('state', new sfValidatorString());
+    $this->setValidator('postcode', new sfValidatorString());
+    $this->setValidator('country', new sfValidatorI18nChoiceCountry());
+    $this->setValidator('phonenumber', new sfValidatorString());
+    $this->setValidator('password2', new sfValidatorString());
   }
 }
